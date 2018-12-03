fd60da49b59c86e5c9666f38e5c045817e0f60d4
Zach North <zrnorth@gmail.com>
2018-12-03 20:40:31 +0900

    Adding some sample stuff: hot reloading, a search bar, dismiss buttons

--- src/App.js
+++ src/App.js
@@ -2,12 +2,91 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const list = [
+  {
+    title: 'React',
+    url: 'https://reactjs.org/',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: 'Redux',
+    url: 'https://redux.js.org/',
+    author: 'Dan Abramov, Andrew Clark',
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+  },
+  {
+    title: 'Big boys club',
+    url: 'https://bigboys.js.org/',
+    author: 'Big Abramov, Andrew Boy',
+    num_comments: 2,
+    points: 5,
+    objectID: 2,
+  },
+];
+
+// Helper function for the search functionality below
+const isSearched = searchTerm => item =>
+  item.title.toLowerCase().includes(searchTerm.toLowerCase());
+
+
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      list,
+      searchTerm: '',
+    };
+    // Don't forget to define all your functions in "this" in the constructor.
+    this.onDismiss = this.onDismiss.bind(this);
+    this.onSearchChange = this.onSearchChange.bind(this);
+    
+  }
+
+  onDismiss(id) {
+    // this is a function object
+    const isNotId = item => item.objectID !== id;
+    const updatedList = this.state.list.filter(isNotId);
+    this.setState({ list: updatedList });
+  }
+
+  onSearchChange(event) {
+    this.setState({ searchTerm: event.target.value });
+  }
+
   render() {
-    const helloWorld = "Welcome to the test app.";
     return (
       <div className="App">
-        <h2>{helloWorld}</h2>
+      <form>
+        <input 
+          type="text"
+          onChange={this.onSearchChange}
+        />
+      </form>
+        {this.state.list.filter(isSearched(this.state.searchTerm)).map(item =>
+          <div key={item.objectID}>
+            <span>
+              <a href={item.url}>{item.title}</a>
+            </span>
+            <span>{item.author}</span>
+            <span>{item.num_comments}</span>
+            <span>{item.points}</span>
+            <span>
+              <button
+                onClick={() => this.onDismiss(item.objectID)}
+                type="button"
+              >
+                Dismiss
+              </button>
+            </span>
+          </div>
+        )}
       </div>
     );
   }

495f47efb8c6b3b64743f4540517a95a83912a60
Zach North <zrnorth@gmail.com>
2018-12-04 23:19:58 +0900

    Added some styling

--- src/App.js
+++ src/App.js
@@ -63,18 +63,22 @@ class App extends Component {
   render() {
     const { searchTerm, list } = this.state;
     return (
-      <div className="App">
-        <Search 
-          value={searchTerm}
-          onChange={this.onSearchChange}
-        >
-          Search
-        </Search>
-        <Table
-          list={list}
-          pattern={searchTerm}
-          onDismiss={this.onDismiss}
-        />
+      <div className="page">
+        <div className="interactions">
+          <div className="App">
+            <Search 
+              value={searchTerm}
+              onChange={this.onSearchChange}
+            >
+              Search
+            </Search>
+            <Table
+              list={list}
+              pattern={searchTerm}
+              onDismiss={this.onDismiss}
+            />
+          </div>
+        </div>
       </div>
     );
   }
@@ -90,18 +94,19 @@ const Search = ({ value, onChange, children }) =>
   </form>
 
 const Table = ({ list, pattern, onDismiss })  =>
-  <div>
+  <div className="table">
     {list.filter(isSearched(pattern)).map(item =>
-      <div key={item.objectID}>
-        <span>
+      <div key={item.objectID} className="table-row">
+        <span style={{width: '40%'}}>
           <a href={item.url}>{item.title}</a>
         </span>
-        <span>{item.author}</span>
-        <span>{item.num_comments}</span>
-        <span>{item.points}</span>
-        <span>
+        <span style={{width: '30%'}}>{item.author}</span>
+        <span style={{width: '10%'}}>{item.num_comments}</span>
+        <span style={{width: '10%'}}>{item.points}</span>
+        <span style={{width: '10%'}}>
           <Button
-            onClick={() => onDismiss(item.objectID)}              
+            onClick={() => onDismiss(item.objectID)}
+            className="button-inline"             
           >
             Dismiss
           </Button>

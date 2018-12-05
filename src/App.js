0d03950ee7b885e78014f398c53a015aab071901
Zach North <zrnorth@gmail.com>
2018-12-06 00:16:56 +0900

    Hooked up the results list to a fake hackernews endpoint

--- src/App.js
+++ src/App.js
@@ -1,38 +1,19 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-const list = [
-  {
-    title: 'React',
-    url: 'https://reactjs.org/',
-    author: 'Jordan Walke',
-    num_comments: 3,
-    points: 4,
-    objectID: 0,
-  },
-  {
-    title: 'Redux',
-    url: 'https://redux.js.org/',
-    author: 'Dan Abramov, Andrew Clark',
-    num_comments: 2,
-    points: 5,
-    objectID: 1,
-  },
-  {
-    title: 'Big boys club',
-    url: 'https://bigboys.js.org/',
-    author: 'Big Abramov, Andrew Boy',
-    num_comments: 2,
-    points: 5,
-    objectID: 2,
-  },
-];
+const DEFAULT_QUERY = 'redux';
+
+const PATH_BASE = 'https://hn.algolia.com/api/v1';
+const PATH_SEARCH = '/search';
+const PARAM_SEARCH = 'query=';
+
+const url = `${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${DEFAULT_QUERY}`;
+
+const list = [];
 
 // Helper function for the search functionality below
 const isSearched = searchTerm => item =>
   item.title.toLowerCase().includes(searchTerm.toLowerCase());
-
 
 class App extends Component {
 
@@ -40,13 +21,18 @@ class App extends Component {
     super(props);
 
     this.state = {
-      list,
-      searchTerm: '',
+      result: null,
+      searchTerm: DEFAULT_QUERY,
     };
-    // Don't forget to define all your functions in "this" in the constructor.
+
+    this.setSearchTopStories = this.setSearchTopStories.bind(this);
     this.onDismiss = this.onDismiss.bind(this);
     this.onSearchChange = this.onSearchChange.bind(this);
-    
+
+  }
+
+  setSearchTopStories(result) {
+    this.setState({ result });
   }
 
   onDismiss(id) {
@@ -60,20 +46,30 @@ class App extends Component {
     this.setState({ searchTerm: event.target.value });
   }
 
+  componentDidMount() {
+    const { searchTerm } = this.state;
+    fetch(`${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}`)
+      .then(response => response.json())
+      .then(result => this.setSearchTopStories(result))
+      .catch(error => error);
+  }
+
   render() {
-    const { searchTerm, list } = this.state;
+    const { searchTerm, result } = this.state;
+
+    if (!result) { return null; }
     return (
       <div className="page">
         <div className="interactions">
           <div className="App">
-            <Search 
+            <Search
               value={searchTerm}
               onChange={this.onSearchChange}
             >
               Search
             </Search>
             <Table
-              list={list}
+              list={result.hits}
               pattern={searchTerm}
               onDismiss={this.onDismiss}
             />
@@ -86,27 +82,27 @@ class App extends Component {
 
 const Search = ({ value, onChange, children }) =>
   <form>
-    {children} <input 
+    {children} <input
       type="text"
       value={value}
       onChange={onChange}
     />
   </form>
 
-const Table = ({ list, pattern, onDismiss })  =>
+const Table = ({ list, pattern, onDismiss }) =>
   <div className="table">
     {list.filter(isSearched(pattern)).map(item =>
       <div key={item.objectID} className="table-row">
-        <span style={{width: '40%'}}>
+        <span style={{ width: '40%' }}>
           <a href={item.url}>{item.title}</a>
         </span>
-        <span style={{width: '30%'}}>{item.author}</span>
-        <span style={{width: '10%'}}>{item.num_comments}</span>
-        <span style={{width: '10%'}}>{item.points}</span>
-        <span style={{width: '10%'}}>
+        <span style={{ width: '30%' }}>{item.author}</span>
+        <span style={{ width: '10%' }}>{item.num_comments}</span>
+        <span style={{ width: '10%' }}>{item.points}</span>
+        <span style={{ width: '10%' }}>
           <Button
             onClick={() => onDismiss(item.objectID)}
-            className="button-inline"             
+            className="button-inline"
           >
             Dismiss
           </Button>
@@ -115,7 +111,7 @@ const Table = ({ list, pattern, onDismiss })  =>
     )}
   </div>
 
-const Button = ({ onClick, className = '', children}) =>
+const Button = ({ onClick, className = '', children }) =>
   <button
     onClick={onClick}
     className={className}

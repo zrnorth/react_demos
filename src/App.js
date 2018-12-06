a64c170832eaaa866ef1992ca813b95e3f0c67f7
Zach North <zrnorth@gmail.com>
2018-12-06 22:23:11 +0900

    actually handle errors

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ const PARAM_SEARCH = 'query=';
 const PARAM_PAGE = 'page=';
 const PARAM_HPP = 'hitsPerPage=';
 
-const url = `${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${DEFAULT_QUERY}&${PARAM_PAGE}`;
+// const url = `${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${DEFAULT_QUERY}&${PARAM_PAGE}`;
 
 class App extends Component {
 
@@ -21,6 +21,7 @@ class App extends Component {
       result: null,
       searchKey: '',
       searchTerm: DEFAULT_QUERY,
+      error: null,
     };
 
     this.needsToSearchTopStories = this.needsToSearchTopStories.bind(this);
@@ -61,7 +62,7 @@ class App extends Component {
     fetch(`${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}&${PARAM_PAGE}${page}&${PARAM_HPP}${DEFAULT_HPP}`)
     .then(response => response.json())
     .then(result => this.setSearchTopStories(result))
-    .catch(error => error);
+    .catch(error => this.setState({ error }));
   }
 
   onSearchChange(event) {
@@ -103,7 +104,8 @@ class App extends Component {
     const {
       searchTerm,
       results,
-      searchKey
+      searchKey,
+      error
     } = this.state;
 
     const page = (
@@ -117,6 +119,7 @@ class App extends Component {
       results[searchKey] &&
       results[searchKey].hits
     ) || [];
+
     return (
       <div className="page">
         <div className="interactions">
@@ -127,10 +130,15 @@ class App extends Component {
           >
             Search
           </Search>
-          <Table
-            list={list}
-            onDismiss={this.onDismiss}
-          />
+          { error
+            ? <div className="interactions">
+              <p>Something went wrong.</p>
+            </div>
+            : <Table
+              list={list}
+              onDismiss={this.onDismiss}
+            />
+          }
         </div>
         <div className="interactions">
           <Button onClick={() => this.fetchSearchTopStories(searchTerm, page + 1)}>

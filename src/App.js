dde5bb4d31fe333089935bfc27afc5fd3fb745ab
Zach North <zrnorth@gmail.com>
2018-12-08 14:43:31 +0900

    Adding auto-focus using ref

--- src/App.js
+++ src/App.js
@@ -157,22 +157,36 @@ class App extends Component {
   }
 }
 
-const Search = ({ 
-  value, 
-  onChange, 
-  onSubmit,
-  children 
-}) =>
-  <form onSubmit={onSubmit}>
-    <input
-      type="text"
-      value={value}
-      onChange={onChange}
-    />
-    <button type="submit">
-      {children}
-    </button>
-  </form>
+class Search extends Component {
+  componentDidMount() {
+    if (this.input) {
+      this.input.focus();
+    }
+  }
+
+  render() {
+    const {
+      value, 
+      onChange, 
+      onSubmit,
+      children,
+    } = this.props;
+
+    return (
+      <form onSubmit={onSubmit}>
+        <input
+          type="text"
+          value={value}
+          onChange={onChange}
+          ref={el => this.input = el}
+        />
+        <button type="submit">
+          {children}
+        </button>
+      </form>
+    )
+  }
+}
 
 Search.propTypes = {
   value: PropTypes.string,
